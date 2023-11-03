3131ee07544c6a4c0a58a0acc02f861a8ae273c4
pwatson100 <paul@skynet2020.co.uk>
2023-11-03 11:15:12 +0000

    TAH 1.5 Compatible

--- scripts/roll-handler.js
+++ scripts/roll-handler.js
@@ -12,7 +12,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 		 * @param {object} event        The event
 		 * @param {string} encodedValue The encoded value
 		 */
-		async doHandleActionEvent(event, encodedValue) {
+		async handleActionClick(event, encodedValue) {
 			const payload = encodedValue.split("|");
 
 			if (payload.length !== 2) {
@@ -25,7 +25,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 			const renderable = [];
 
 			if (renderable.includes(actionTypeId) && this.isRenderItem()) {
-				return this.doRenderItem(this.actor, actionId);
+				return this.rRenderItem(this.actor, actionId);
 			}
 
 			const knownCharacters = ["character", "synthetic", "creature"];
@@ -74,7 +74,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 											await this.#handleUArmorAction(event, actor, actionId, actorItem);
 											break;
 										default:
-											await this.doRenderItem(this.actor, actionId);
+											await this.renderItem(this.actor, actionId);
 											break;
 									}
 								}
@@ -97,7 +97,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 								break;
 
 							default:
-								await this.doRenderItem(this.actor, actionId);
+								await this.renderItem(this.actor, actionId);
 								break;
 						}
 					}
@@ -136,7 +136,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 										await this.#handleWeaponAction(event, actor, actionId, actorItem);
 										break;
 									default:
-										await this.doRenderItem(this.actor, actionId);
+										await this.renderItem(this.actor, actionId);
 										break;
 								}
 							}
@@ -163,7 +163,7 @@ Hooks.once("tokenActionHudCoreApiReady", async (coreModule) => {
 										await this.#handleWeaponAction(event, actor, actionId, actorItem);
 										break;
 									default:
-										await this.doRenderItem(this.actor, actionId);
+										await this.renderItem(this.actor, actionId);
 										break;
 								}
 							}
